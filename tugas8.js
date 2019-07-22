48334a388bcccbe3015a60aeedc676b6cea26250
Martino Wagey <martinowagey@gmail.com>
2019-07-22 16:53:58 +0700

    add tugas 8 JS

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function tugas8(){
+  var profil = {
+      fname: "Martino",
+      lname: "Wagey",
+      umur: "22 Tahun",
+  }
+console.log(profil);
+profil.umur="20 Tahun"
+console.log(profil);
+}
+
+tugas8();
